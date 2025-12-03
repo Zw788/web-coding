eea75b3163a8ca215803f948916d4c2d17398334
Zw788 <31865624+Zw788@users.noreply.github.com>
2025-12-03 22:21:48 +0000

    day3

--- testing.cpp
+++ testing.cpp
@@ -1,14 +1,19 @@
 /*
 "stage changes" , no, push directly
-"commit and sync" , always
+"commit and sync" , always3
+"pull?" doesn't matter
+
+
+run in terminal "cd /workspaces/web-coding && g++ -Wall -Wextra -g3 testing.cpp -o output/testing && cd output && ./testing"
 */
-
-
 
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
+#include <chrono>
+#include <set>
+#include <cmath>
 
 
 using namespace std;
@@ -16,7 +21,25 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello, World!" << endl;
-	
+	auto start = chrono::steady_clock::now();
+	/////////////////////////////////////////////////////////
+	ifstream myfile;
+	myfile.open("../aoc2025_input.txt");
+	if (!myfile.is_open()) {
+		cerr << "Error opening file" << endl;
+		return 1;
+	}
+
+	string line;
+	while (getline(myfile,line)) {
+		cout << line << "\n";
+	}
+
+	myfile.close();
+	/////////////////////////////////////////////////////////
+	auto end = chrono::steady_clock::now();
+	auto diff = end - start;
+	cout << "Execution time: " << chrono::duration<double, milli>(diff).count() << " ms" << endl;
+
 	return 0;
 }
